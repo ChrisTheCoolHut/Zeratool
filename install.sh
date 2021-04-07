6e3fe3adc1caf62298640749a2013e1b914d1de8
hdthky <hdthky@outlook.com>
2021-04-07 05:19:13 -0700

    Fixed old dependencies

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt-get install python-pip python-dev build-essential rubygems-integration ruby-dev rubygems python-dev libffi-dev -y
+sudo apt-get install python-pip build-essential rubygems-integration ruby-dev rubygems python-dev libffi-dev pkg-config wget -y
 #Ubuntu 12 -> rubygems
 #Ubuntu 14 -> rubygems-integration
 #Ubuntu 16,18 -> ruby-dev
@@ -8,10 +8,9 @@ sudo dpkg --add-architecture i386
 sudo apt-get update
 sudo apt-get install libc6:i386 libstdc++6:i386 -y
 
- 
+sudo pip install --upgrade pip==19.0.0 
+
 sudo pip install virtualenv virtualenvwrapper
- 
-sudo pip install --upgrade pip
   
 printf '\n%s\n%s\n%s' '# virtualenv' 'export WORKON_HOME=~/virtualenvs' 'source /usr/local/bin/virtualenvwrapper.sh' >> ~/.bashrc
 
@@ -25,13 +24,13 @@ workon zeratool
 sudo gem install one_gadget
 
 #Need to port to latest angr
-pip install angr==7.8.2.21 cffi==1.7.0 future==0.16.0 pycparser==2.18  IPython==5.0 r2pipe psutil timeout_decorator pwn
+pip install angr==7.8.2.21 pwntools==3.13.0 ropper==1.13.6 cffi==1.7.0 future==0.16.0 pycparser==2.18  IPython==5.0 r2pipe==1.4.2 psutil==5.8.0 timeout_decorator==0.5.0
 
 git clone https://github.com/radare/radare2.git
 
-sudo ./radare2/sys/install.sh
-
-pip install IPython==5.0 r2pipe psutil timeout_decorator pwn
+cd radare2
+git checkout 5.1.0
+sudo ./sys/install.sh
 
 echo "####################"
 echo "run: . ~/.bashrc"
